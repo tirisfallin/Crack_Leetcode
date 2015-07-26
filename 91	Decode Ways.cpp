cbaff1cf726d2aa933c76eb33bdf2d180ff90d3a
zjlin1212 <tirisfallin@gmail.com>
2015-07-27 00:09:24 +0800

    Update 91	Decode Ways.cpp

--- 91	Decode Ways.cpp
+++ 91	Decode Ways.cpp
@@ -1,35 +1,41 @@
 class Solution {
 public:
-    int decodehelper(string& s,int begin){
-        if(s.size()-begin>0&&s[begin]=='0')
+    int decodehelper(string& s,int begin,int end){
+        if(begin>end)
             return 0;
-        if(s.size()-begin==1||s.size()-begin==0){
+        if(end-begin>0&&s[begin]=='0')
+            return 0;
+        if(end-begin==1||end-begin==0){
             return 1;
         }
-        if(s[begin]>='3')
-          return decodehelper(s,begin+1);
+        if(end-begin==2){
+            if(10*s[begin]+s[begin+1]-11*'0'<=26)
+                return decodehelper(s, begin+1,end)+decodehelper(s, begin+2,end);
+            else
+                return decodehelper(s,begin+1,end);
+        }
+        int length=(end-begin)/2;
+        while(length>0&&begin+length<end){
+            if(s[begin+length]>='3')
+                break;
+            else
+                length--;
+        }
+        if(length>0&&begin+length<end)
+        {
+            string temp=s.substr(begin,length+1);
+            return decodehelper(s, begin+length+1,end)*decodehelper(temp, 0,temp.size());
+        }
+        
         if(10*s[begin]+s[begin+1]-11*'0'<=26)
-            return decodehelper(s, begin+1)+decodehelper(s, begin+2);
+            return decodehelper(s, begin+1,end)+decodehelper(s, begin+2,end);
         else
-            return decodehelper(s,begin+1);
+            return decodehelper(s,begin+1,end);
             
     }
     int numDecodings(string s) {
-        int begin=s.size()/2;
-        string temp;
         if(s.empty())
             return 0;
-        while(begin>=0){
-            if(s[begin]>='3')
-                break;
-            else
-                begin--;
-        }
-        if(begin>0)
-        {
-            temp=s.substr(0,begin+1);
-            return decodehelper(s, begin+1)*decodehelper(temp, 0);
-        }
-        else return decodehelper(s, 0);
+        else return decodehelper(s, 0,s.size());
     }
 };
